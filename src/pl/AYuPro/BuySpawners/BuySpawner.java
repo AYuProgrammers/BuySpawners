1a74496a3652da967fa706f6b3a6a795ccb828da
AYuPro <ayuprogrammers@yandex.ru>
2014-03-31 07:07:36 -0700

    Update BuySpawner.java

--- src/pl/AYuPro/BuySpawners/BuySpawner.java
+++ src/pl/AYuPro/BuySpawners/BuySpawner.java
@@ -54,37 +54,35 @@ public class BuySpawner extends JavaPlugin {
 	public void onDisable() {
 		Bukkit.getServer().getLogger().info("BuySpawners by AYuPro Unloaded");
 	}
-	
-	
 
 	public boolean onCommand(CommandSender sender, Command cmd, String CommandLabel, String[] args) {
 		if (!(sender.hasPermission("spawner.cmd"))){
-			sender.sendMessage(ChatColor.RED + "У Вас не достаточно прав.");
+			sender.sendMessage(ChatColor.RED + "РЈ Р’Р°СЃ РЅРµ РґРѕСЃС‚Р°С‚РѕС‡РЅРѕ РїСЂР°РІ.");
 			return true;
 		}
 		if (cmd.getName().equalsIgnoreCase("spawner")) {
 			if (!(sender instanceof Player)) {
-				sender.sendMessage(ChatColor.RED + "Эта команда не работает в консоли.");
+				sender.sendMessage(ChatColor.RED + "Р­С‚Р° РєРѕРјР°РЅРґР° РЅРµ СЂР°Р±РѕС‚Р°РµС‚ РІ РєРѕРЅСЃРѕР»Рё.");
 				return true;
 			} else {
 				Player player = (Player) sender;
 				try {
 					arg = args[0];
 				} catch (Exception e) {
-					sender.sendMessage(ChatColor.RED + "Вы должны указать тип моба.");
+					sender.sendMessage(ChatColor.RED + "Р’С‹ РґРѕР»Р¶РЅС‹ СѓРєР°Р·Р°С‚СЊ С‚РёРї РјРѕР±Р°.");
 					return true;
 				}
 				if (args[0].toLowerCase().equals("help")){
-				     sender.sendMessage(ChatColor.RED + "========== Список возможных типов мобов: ==========");
-				     sender.sendMessage(ChatColor.GOLD + "pig - свинья");
-				     sender.sendMessage(ChatColor.GOLD + "wolf - волк");
-				     sender.sendMessage(ChatColor.GOLD + "zombie - зомби");
-				     sender.sendMessage(ChatColor.GOLD + "skeleton - скелет");
-				     sender.sendMessage(ChatColor.GOLD + "spider - паук");
-				     sender.sendMessage(ChatColor.GOLD + "pig_zombie - свинозомби");
-				     sender.sendMessage(ChatColor.GOLD + "creeper - крипер");
-				     sender.sendMessage(ChatColor.GOLD + "enderman - эндермен, странник края");
-				     sender.sendMessage(ChatColor.GOLD + "blaze - ифрит");
+				     sender.sendMessage(ChatColor.RED + "========== РЎРїРёСЃРѕРє РІРѕР·РјРѕР¶РЅС‹С… С‚РёРїРѕРІ РјРѕР±РѕРІ: ==========");
+				     sender.sendMessage(ChatColor.GOLD + "pig - СЃРІРёРЅСЊСЏ");
+				     sender.sendMessage(ChatColor.GOLD + "wolf - РІРѕР»Рє");
+				     sender.sendMessage(ChatColor.GOLD + "zombie - Р·РѕРјР±Рё");
+				     sender.sendMessage(ChatColor.GOLD + "skeleton - СЃРєРµР»РµС‚");
+				     sender.sendMessage(ChatColor.GOLD + "spider - РїР°СѓРє");
+				     sender.sendMessage(ChatColor.GOLD + "pig_zombie - СЃРІРёРЅРѕР·РѕРјР±Рё");
+				     sender.sendMessage(ChatColor.GOLD + "creeper - РєСЂРёРїРµСЂ");
+				     sender.sendMessage(ChatColor.GOLD + "enderman - СЌРЅРґРµСЂРјРµРЅ, СЃС‚СЂР°РЅРЅРёРє РєСЂР°СЏ");
+				     sender.sendMessage(ChatColor.GOLD + "blaze - РёС„СЂРёС‚");
 				     return true;
 				}
 				CreatureType crtype;
@@ -92,21 +90,21 @@ public class BuySpawner extends JavaPlugin {
 				try {
 					crtype = CreatureType.valueOf(args[0].toUpperCase());
 				} catch (Exception e) {
-					sender.sendMessage(ChatColor.RED + "Неправильное название существа, воспользуйтесь справкой /spawner help");
+					sender.sendMessage(ChatColor.RED + "РќРµРїСЂР°РІРёР»СЊРЅРѕРµ РЅР°Р·РІР°РЅРёРµ СЃСѓС‰РµСЃС‚РІР°, РІРѕСЃРїРѕР»СЊР·СѓР№С‚РµСЃСЊ СЃРїСЂР°РІРєРѕР№ /spawner help");
 					return true;
 				}
 				
 				Block b = player.getLocation().getBlock();
 				
 				if (!(wgPlugin.canBuild(player, b))){
-					sender.sendMessage(ChatColor.RED + "Для установки спавнера у Вас должны быть права в регионе.");
+					sender.sendMessage(ChatColor.RED + "Р”Р»СЏ СѓСЃС‚Р°РЅРѕРІРєРё СЃРїР°РІРЅРµСЂР° Сѓ Р’Р°СЃ РґРѕР»Р¶РЅС‹ Р±С‹С‚СЊ РїСЂР°РІР° РІ СЂРµРіРёРѕРЅРµ.");
 					return true;
 				}
 
 				if (getConfig().getBoolean("spawner." + arg.toLowerCase() + ".enabled")||player.isOp()) {
 					if (getConfig().getBoolean("spawner." + arg.toLowerCase() + ".vip")){
 						if (!(sender.hasPermission("spawner.vip"))){
-							sender.sendMessage(ChatColor.RED + "У Вас не достаточно прав.");
+							sender.sendMessage(ChatColor.RED + "РЈ Р’Р°СЃ РЅРµ РґРѕСЃС‚Р°С‚РѕС‡РЅРѕ РїСЂР°РІ.");
 							return true;
 						}
 					}
@@ -116,38 +114,24 @@ public class BuySpawner extends JavaPlugin {
 							b.setType(Material.MOB_SPAWNER);
 							CreatureSpawner s = (CreatureSpawner) b.getState();
 							s.setCreatureType(crtype);
-							sender.sendMessage(ChatColor.GREEN + "Спавнер " + arg.toLowerCase() + "(s) установлен.");
+							sender.sendMessage(ChatColor.GREEN + "РЎРїР°РІРЅРµСЂ " + arg.toLowerCase() + "(s) СѓСЃС‚Р°РЅРѕРІР»РµРЅ.");
 							return true;
 						} catch (Exception e){
 							econ.depositPlayer(player.getName(), getConfig().getInt("spawner." + arg.toLowerCase() + ".price"));
-							sender.sendMessage(ChatColor.RED + "Спавнер не был установлен из-за ошибки.");
+							sender.sendMessage(ChatColor.RED + "РЎРїР°РІРЅРµСЂ РЅРµ Р±С‹Р» СѓСЃС‚Р°РЅРѕРІР»РµРЅ РёР·-Р·Р° РѕС€РёР±РєРё.");
 							return true;
 						}
 						
 					} else {
-						sender.sendMessage(ChatColor.RED + "У Вас недостаточно средств.");
+						sender.sendMessage(ChatColor.RED + "РЈ Р’Р°СЃ РЅРµРґРѕСЃС‚Р°С‚РѕС‡РЅРѕ СЃСЂРµРґСЃС‚РІ.");
 						return true;
 					}
 				} else {
-					sender.sendMessage(ChatColor.RED + "Этот тип мобов в списке отключенных или недоступных.");
+					sender.sendMessage(ChatColor.RED + "Р­С‚РѕС‚ С‚РёРї РјРѕР±РѕРІ РІ СЃРїРёСЃРєРµ РѕС‚РєР»СЋС‡РµРЅРЅС‹С… РёР»Рё РЅРµРґРѕСЃС‚СѓРїРЅС‹С….");
 					return true;
 				}
 			}
 		}
-		
-		
-		
 		return false;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
